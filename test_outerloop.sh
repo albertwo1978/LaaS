6275c9ac62382292f905b4063ca7d4e02cddc828
daweins <daweins@Microsoft.com>
2019-12-23 14:53:01 +0000

    outer loop iteration success

--- test_outerloop.sh
+++ test_outerloop.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
 # Testing outer loop
 
+# Debug Settings
+doBlobUpdate=0 # 0 = disable
+
 testSubID='33e681ce-910a-44b4-84b8-d07290144803'
 testSAName='storannandale'
 testContainerName='testcfg'
 tempTestDir='temp/testDefs'
+testMasterName='currentTests.csv'
+
+
 echo Start
 blobJSON=`az storage blob list -c $testContainerName --account-name $testSAName --subscription $testSubID | jq '.[].name'`
 
-for curBlobName in $blobJSON
+# Download the Test Definitions from Blob Storage
+if [ $doBlobUpdate -ne 0 ] 
+then
+    for curBlobName in $blobJSON
+    do
+        cleanBlobName=`sed 's/"//g' <<< $curBlobName` 
+        # download the blob to temp
+        curBlobDestFile=$tempTestDir/$cleanBlobName
+        echo $curBlobDestFile
+        az storage blob download --container-name $testContainerName --account-name $testSAName --subscription $testSubID -n $cleanBlobName -f $curBlobDestFile
+
+    done
+fi
+# Read the Master Test Config
+testMasterFile=$tempTestDir/$testMasterName
+masterConfig=`cat $testMasterFile`
+for curTestLine in $masterConfig
 do
-    cleanBlobName=`sed 's/"//g' <<< $curBlobName` 
-    # download the blob to temp
-    curBlobDestFile=$tempTestDir/$cleanBlobName
-    echo $curBlobDestFile
-    az storage blob download --container-name $testContainerName --account-name $testSAName --subscription $testSubID -n $cleanBlobName -f $curBlobDestFile
-
+    # TODO - ignore first line
+    echo $curTestLine
+    IFS=',' read -ra curTestArray  <<< $curTestLine
+    curTestName=${curTestArray[0]}
+    curTestEnabled=${curTestArray[1]}
+    curTestURL=${curTestArray[2]}
+    curTestParam=${curTestArray[3]}
+    curTestLoad=${curTestArray[4]}
+    
 done
-
