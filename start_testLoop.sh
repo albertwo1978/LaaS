f1f443534dd1fb89131a5b0c73359021e2dd9716
daweins <daweins@Microsoft.com>
2019-12-26 16:29:06 +0000

    added yaml back

--- start_testLoop.sh
+++ start_testLoop.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 # Testing outer loop
 
 # Debug Settings
 doBlobUpdate=0 # 0 = disable
+doK8s=1 # 0 = disable
+
+
+integerCheck='^[0-9]+$'
 
 testSubID='33e681ce-910a-44b4-84b8-d07290144803'
 testSAName='storannandale'
@@ -10,8 +14,15 @@ testContainerName='testcfg'
 tempTestDir='temp/testDefs'
 testMasterName='currentTests.csv'
 
+jmxRunFile='temp/currentJmxRun.jmx'
+jmxFile='jmx/baseline.jmx'
+working_dir='k8sDefs'
+# obsolete
+#if [ $doK8s -ne 0 ]
+#then 
+#    ./start_jmeterPods.sh
+#fi
 
-echo Start
 blobJSON=`az storage blob list -c $testContainerName --account-name $testSAName --subscription $testSubID | jq '.[].name'`
 
 # Download the Test Definitions from Blob Storage
@@ -29,16 +40,94 @@ then
 fi
 # Read the Master Test Config
 testMasterFile=$tempTestDir/$testMasterName
+IFS=$'\n'
 masterConfig=`cat $testMasterFile`
 for curTestLine in $masterConfig
 do
     # TODO - ignore first line
     echo $curTestLine
-    IFS=',' read -ra curTestArray  <<< $curTestLine
-    curTestName=${curTestArray[0]}
-    curTestEnabled=${curTestArray[1]}
-    curTestURL=${curTestArray[2]}
-    curTestParam=${curTestArray[3]}
-    curTestLoad=${curTestArray[4]}
-    
+    if [[ $curTestLine == CurTest* ]]
+    then
+        echo Skipping First Line of Test List
+    else
+        IFS=',' read -ra curTestArray  <<< $curTestLine
+        curTestName=${curTestArray[0]}
+        curTestEnabled=${curTestArray[1]}
+        curTestURL=${curTestArray[2]}
+        curTestParam=${curTestArray[3]}
+        curTestLoadName=${curTestArray[4]}
+        curTestFile=$tempTestDir/$curTestLoadName
+        curTestFile=`echo $curTestFile | tr --delete '\r'`  # Windows line endings are the devil
+        echo "Reading definition from $curTestFile"
+
+        IFS=$'\n'
+        curTestDefText=`cat $curTestFile`
+        for curTestDefLine in $curTestDefText
+        do
+            IFS=',' read -ra testArray <<< "$curTestDefLine"
+            numUsers=`echo ${testArray[0]} | tr --delete '\r'`
+            duration=`echo ${testArray[1]} | tr --delete '\r'`
+            ramp=`echo ${testArray[2]} | tr --delete '\r'`
+            
+            if [[ $numUsers == NumUsers* ]]
+            then
+                echo Skipping First Line of Test Def
+            else
+               
+
+                #DEBUG
+                duration=10
+                echo $curTestDefLine
+            
+                # Feed integer string into jmx file and run test
+                if ! [[ $numUsers =~ $integerCheck && $duration =~ $integerCheck && $ramp =~ $integerCheck ]]
+                then
+                    echo "Non-integer input detected. Skipping line." 
+                else
+                    # Script created to launch Jmeter tests directly from the current terminal without accessing the jmeter master pod.
+                    # It requires that you supply the path to the jmx file
+                    ((throughputPerMin=$numUsers*60)) 
+                    echo $numUsers
+                    echo $duration
+                    echo $ramp
+                    echo $throughputPerMin
+                
+                
+
+
+                    sedString="s/{numUsers}/$numUsers/g;s/{duration}/$duration/g;s/{ramp}/$ramp/g;s/{throughputPerMin}/$throughputPerMin/g"
+                    
+                    
+                    #echo $sedString
+
+                    sed $sedString $jmxFile > $jmxRunFile
+
+                    test_name="$(basename "$jmxRunFile")"
+
+
+                    if [ $doK8s -ne 0 ]
+                    then 
+                        # TODO - check to see if the namespace already exists
+                        workloadTenant=$curTestName
+                        kubectl create namespace $workloadTenant
+
+                        echo "Namspace $workloadTenant has been created"
+
+                        # Create  Master pod details
+                        echo "Creating Jmeter Master"
+                        kubectl create -n $workloadTenant -f $working_dir/jmeter_master_configmap.yaml
+
+                        kubectl create -n $workloadTenant -f $working_dir/jmeter_master_deploy.yaml
+                        kubectl cp "$jmxRunFile" -n $workloadTenant "$master_pod:/$test_name"
+
+                        # Starting Jmeter load test
+
+                        kubectl exec -ti -n $workloadTenant $master_pod -- /bin/bash /load_test "$test_name"
+                    fi
+                fi
+            fi
+        done
+    fi
+
+
 done
