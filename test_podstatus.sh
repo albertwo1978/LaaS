d3043158a3a9d83f14dfeba95f16d3eceb779ffc
daweins <daweins@Microsoft.com>
2020-01-03 13:15:23 +0000

    cleanup

--- test_podstatus.sh
+++
@@ -1,9 +0,0 @@
-kubectl -n azcatsynthappbaseline get pods
-podStatus=`kubectl -n azcatsynthappbaseline get pods`
-if [[ $podStatus == *Running* ]]
-then
-    echo Yes
-else
-    echo No
-fi
-echo 'Done'
