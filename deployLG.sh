26db3e9dd863df9901fdc6fe14320ce79f3c4701
daweins <daweins@Microsoft.com>
2020-02-04 00:45:19 +0000

    Perms

--- deployLG.sh
+++ deployLG.sh
@@ -1,7 +1,8 @@
 myNamespace='orchestrator'
+accountYAML='k8sDefs/kubeorchestratoraccount.yaml'
 deployYAML='k8sDefs/kubeorchestrator.yaml'
 
 kubectl delete namespace $myNamespace
 kubectl create namespace $myNamespace
+kubectl create -n $myNamespace -f $accountYAML
 kubectl create -n $myNamespace -f $deployYAML
-
